b279ce5c8bf6aa7f4d67df62643109b134859c19
Maxwell Muoto <muotomax@email.unc.edu>
2021-05-20 13:54:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google Chrome QR Code Scanner
+
+Scans all QR codes on a website or PDF opened in Chrome. Utilizes [nimiq/qr-scanner](https://github.com/nimiq/qr-scanner) to read the QR code.
---
+++ src/scan.js
@@ -0,0 +1,17 @@
+import QrScanner from './scanner/qr-scanner.min.js';
+QrScanner.WORKER_PATH = './scanner/qr-scanner-worker.min.js';
+
+
+const scan = (image) => {
+  QrScanner.scanImage(image)
+  .then(result => console.log(result) & update_link(result))
+  .catch(error => console.log(error || 'No QR code found.'));
+}
+
+
+chrome.tabs.captureVisibleTab(undefined, { format: 'jpeg' }, scan)
+const update_link = (result) => {
+  var click = document.getElementById("link");
+  click.innerHTML = result;
+  click.setAttribute("href", result)
+}
