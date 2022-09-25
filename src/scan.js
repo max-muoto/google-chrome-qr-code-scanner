5774b184ef6e5b7cca37c418a85312cae484b0b5
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-09-24 23:48:18 -0400

    Format with Prettier

--- src/scan.js
+++ src/scan.js
@@ -1,17 +1,15 @@
-import QrScanner from './scanner/qr-scanner.min.js';
-QrScanner.WORKER_PATH = './scanner/qr-scanner-worker.min.js';
-
+import QrScanner from "./scanner/qr-scanner.min.js";
+QrScanner.WORKER_PATH = "./scanner/qr-scanner-worker.min.js";
 
 const scan = (image) => {
   QrScanner.scanImage(image)
-  .then(result => console.log(result) & update_link(result))
-  .catch(error => console.log(error || 'No QR code found.'));
-}
+    .then((result) => console.log(result) & update_link(result))
+    .catch((error) => console.log(error || "No QR code found."));
+};
 
-
-chrome.tabs.captureVisibleTab(undefined, { format: 'jpeg' }, scan)
+chrome.tabs.captureVisibleTab(undefined, { format: "jpeg" }, scan);
 const update_link = (result) => {
   var click = document.getElementById("link");
   click.innerHTML = result;
-  click.setAttribute("href", result)
-}
+  click.setAttribute("href", result);
+};
