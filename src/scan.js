25a1db5ffb9502ccd66a43ffb4a25f9a3ef63035
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-09-24 23:55:55 -0400

    Small code changes

--- src/scan.js
+++ src/scan.js
@@ -1,12 +1,14 @@
 import QrScanner from "./scanner/qr-scanner.min.js";
 QrScanner.WORKER_PATH = "./scanner/qr-scanner-worker.min.js";
 
-const scan = (image) => {
-  QrScanner.scanImage(image)
-    .then((result) => console.log(result) & update_link(result))
+// Scans a given picture for a QR code.
+const scan = (current_tab_picture) => {
+  QrScanner.scanImage(current_tab_picture)
+    .then((result) => update_link(result))
     .catch((error) => console.log(error || "No QR code found."));
 };
 
+// Take picture of the current tab, and scan for a QR code.
 chrome.tabs.captureVisibleTab(undefined, { format: "jpeg" }, scan);
 const update_link = (result) => {
   var click = document.getElementById("link");
