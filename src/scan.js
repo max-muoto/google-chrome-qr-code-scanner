a266e204e2918ca4e9b784c4902f7f7417b183eb
max-muoto <41130755+max-muoto@users.noreply.github.com>
2023-02-20 15:04:12 -0600

    Minor cleanup

--- src/scan.js
+++ src/scan.js
@@ -2,27 +2,27 @@ import QrScanner from "./scanner/qr-scanner.min.js";
 QrScanner.WORKER_PATH = "./scanner/qr-scanner-worker.min.js";
 
 // Scans a given picture for a QR code.
-const scan = (current_tab_picture) => {
-  QrScanner.scanImage(current_tab_picture)
-    .then((result) => update_link(result))
-    .catch(() => update_no_qr_code());
+const scan = (currentTabPicture) => {
+  QrScanner.scanImage(currentTabPicture)
+    .then((result) => updateLink(result))
+    .catch(() => handleNoQRCode());
 };
 
 // Take picture of the current tab, and scan for a QR code.
 chrome.tabs.captureVisibleTab(undefined, { format: "jpeg" }, scan);
 
 // Update the link for the QR code.
-const update_link = (result) => {
+const updateLink = (result) => {
   var prefix = document.getElementById("prefix");
-  var qr_code_output = document.getElementById("qr-code-link");
+  var qrCodeOutput = document.getElementById("qr-code-link");
   prefix.innerText = "Link:";
-  qr_code_output.innerText = result.length > 25 ? result.substring(0, 25) + "..." : result;
-  qr_code_output.setAttribute("href", result);
-  qr_code_output.setAttribute("target", "_blank");
+  qrCodeOutput.innerText = result.length > 25 ? result.substring(0, 25) + "..." : result;
+  qrCodeOutput.setAttribute("href", result);
+  qrCodeOutput.setAttribute("target", "_blank");
 };
 
 // Handler for when there is no QR code.
-const update_no_qr_code = () => {
+const handleNoQRCode = () => {
   var main_text = document.getElementById("main-text");
   main_text.innerText = "No QR Code Found";
 };
